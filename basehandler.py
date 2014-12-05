b4301c891b58949786cb32414e5646f0130d2dd1
Yoinkbird <yoinkbird2010@gmail.com>
2014-12-05 01:08:37 -0600

    BaseHandler - method for  post-json and get-URLvar

--- basehandler.py
+++ basehandler.py
@@ -53,3 +53,98 @@ class BaseHandler(webapp2.RequestHandler):
           templateStr = common_functions.load_template(self, file=templatePath)
       # print template
       self.response.write(templateStr)
+
+    ################################################################
+    # < def_sendJson>
+    # TODO: split into two functions:
+    #     - one purely for setting up url, args, etc
+    #     - one purely for sending json/retrieving result
+    def sendJson(self,**kwargs):
+      from google.appengine.api import urlfetch
+      urlfetch.set_default_fetch_deadline(60)
+      # define URL as current host
+      url = self.request.host_url + '/'
+      if(kwargs):
+        #TODO: for loop, defaults, error checkign
+        if('url' in kwargs):
+          url = kwargs['url']
+      formDict = kwargs
+      jsondata = formDict['jsondata']
+
+      # check if input data is json
+      # if not, convert to json - this makes calling the function much simpler
+      try:
+        json.loads(jsondata)
+      except:
+        jsondata = json.dumps(jsondata)
+
+      # get "service name", i.e. the url sub-path
+      if('service_name' in formDict):
+        url += formDict['service_name']
+      # src: https://developers.google.com/appengine/docs/python/appidentity/#Python_Asserting_identity_to_Google_APIs
+      result = urlfetch.fetch(
+          url,
+          payload = jsondata,
+          method=urlfetch.POST,
+          headers = {'Content-Type' : "application/json"},
+          )
+      # store return string
+      jsonRetStr = 'the_if_else_broke_in_def_sendjson'
+      if(result.status_code == 200):
+        jsonRetStr = result.content
+      else:
+        jsonRetStr = ("Call failed. Status code %s. Body %s" % (result.status_code, result.content))
+        # Note on error-handling from above google page: # raise Exception(jsonRetStr)
+        jsonRetStr = json.dumps({'error':jsonRetStr})
+      #TODO: validate response with "try: ... except: ..." etc #jsonRetStr = json.loads(result.content)
+      return jsonRetStr
+    # </def_sendJson>
+    ################################################################
+
+    ################################################################
+    # < def_sendUrlGet>
+    # TODO: split into two functions:
+    #     - one purely for setting up url, args, etc
+    #     - one purely for sending json/retrieving result
+    def sendUrlGet(self,**kwargs):
+      from google.appengine.api import urlfetch
+      urlfetch.set_default_fetch_deadline(60)
+      # define URL as current host
+      url = self.request.host_url + '/'
+      if(kwargs):
+        #TODO: for loop, defaults, error checkign
+        if('url' in kwargs):
+          url = kwargs['url']
+      formDict = kwargs
+      jsondata = formDict['jsondata']
+
+      # encode params for URL GET request
+      import urllib
+      requestParams = urllib.urlencode(jsondata)
+
+      url += '/'
+      # get "service name", i.e. the url sub-path
+      if('service_name' in formDict):
+          url += formDict['service_name']
+      if(requestParams):
+        url += '?'
+        url += requestParams
+      # src: https://developers.google.com/appengine/docs/python/appidentity/#Python_Asserting_identity_to_Google_APIs
+      result = urlfetch.fetch(
+          url,
+          #payload = jsondata,
+          method=urlfetch.GET,
+          headers = {'Content-Type' : "application/json"},
+          )
+      # store return string
+      jsonRetStr = 'the_if_else_broke_in_def_sendjson'
+      if(result.status_code == 200):
+        jsonRetStr = result.content
+      else:
+        jsonRetStr = ("Call failed. Status code %s. Body %s" % (result.status_code, result.content))
+        # Note on error-handling from above google page: # raise Exception(jsonRetStr)
+        jsonRetStr = json.dumps({'error':jsonRetStr})
+      #TODO: validate response with "try: ... except: ..." etc #jsonRetStr = json.loads(result.content)
+      return jsonRetStr
+    # </def_sendUrlGet>
+    ################################################################
